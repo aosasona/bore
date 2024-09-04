2eb3d6495183734a03b979d202eeacb8d0a0282a
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-04 20:44:04 +0100

    Updated table structure (requires DB resetting)

--- sql/migrations/0002_create_artifacts_table.up.sql
+++ sql/migrations/0002_create_artifacts_table.up.sql
@@ -1,12 +1,12 @@
-create table artifacts (
-  id text primary key default (uuid()), -- uuid
-  content blob not null,
-  content_sha256 text not null,
-  type text not null default 'text',
-  last_modified integer not null default (unixepoch()),
+CREATE TABLE artifacts (
+  id TEXT primary key default (uuid()), -- uuid
+  content BLOB NOT NULL,
+  content_sha256 TEXT NOT NULL,
+  type TEXT NOT NULL DEFAULT 'text',
+  last_modified INTEGER NOT NULL DEFAULT (unixepoch()),
 
-  collection_id text,
-  foreign key (collection_id) references collections(id)
+  collection_id TEXT,
+  FOREIGN KEY (collection_id) REFERENCES collections(id)
 ) strict;
 
-create unique index idx_artifacts_sha256_collection on artifacts(content_sha256, COALESCE(collection_id, 'root'));
+CREATE UNIQUE INDEX idx_artifacts_sha256_collection ON artifacts(content_sha256, COALESCE(collection_id, 'root'));
