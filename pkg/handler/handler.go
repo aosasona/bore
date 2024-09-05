8cc27a97589ff3410a8265a57bb2663fad59ae95
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-05 22:47:29 +0100

    Added support for format decoding

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
+	"fmt"
 	"io"
 	"time"
 
@@ -10,18 +12,25 @@ import (
 )
 
 const (
-	ArtifactTypeText = "text/plain"
+	FormatBase64    = "base64"
+	FormatPlainText = "plain-text"
 )
 
 type CopyOpts struct {
+	// Collection ID to associate the copied content with
 	CollectionId string
-	ArtifactType string
+
+	// Format of the content to copy
+	Format string
 }
 
 type HandlerInterface interface {
 	Copy(r io.Reader, opts CopyOpts) (string, error)
 
 	PasteLastCopied(io.Writer) error
+
+	// DecodeToFormat decodes the content to the specified format
+	DecodeToFormat([]byte, string) ([]byte, error)
 
 	// TODO: add a PasteManyIdx method that returns a list of artifacts with their numeric index from the bottom (which is then mapped to their UUID ids) with 0 being most recent
 }
@@ -36,19 +45,24 @@ func New(dao *daos.Queries) *Handler {
 
 // Copy copies the content of the reader to the database and returns the ID of the content
 func (h *Handler) Copy(r io.Reader, opts CopyOpts) (string, error) {
+	if !ValidateFormat(opts.Format) {
+		return "", fmt.Errorf("unsupported format: %s", opts.Format)
+	}
+
 	content, err := io.ReadAll(r)
 	if err != nil {
 		return "", nil
 	}
 
+	if content, err = h.DecodeToFormat(content, opts.Format); err != nil {
+		return "", err
+	}
+
 	// Check if the content already exists, if it does, just update the last modified time
 	// TODO: write to the native clipboard regardless if enabled
-
 	ctx, _ := context.WithTimeout(context.TODO(), 10*time.Second)
 
-	createArtifactParams := daos.UpsertArtifactParams{
-		Content: content,
-	}
+	createArtifactParams := daos.UpsertArtifactParams{Content: content}
 	if opts.CollectionId != "" {
 		createArtifactParams.CollectionID = sql.NullString{String: opts.CollectionId, Valid: true}
 	}
@@ -75,6 +89,26 @@ func (h *Handler) PasteLastCopied(w io.Writer) error {
 
 	_, err = w.Write(artifact.Content)
 	return err
+}
+
+// DecodeToFormat decodes the content to the specified (and supported) format
+func (h *Handler) DecodeToFormat(content []byte, format string) ([]byte, error) {
+	switch format {
+	case FormatBase64:
+		destination := make([]byte, base64.StdEncoding.DecodedLen(len(content)))
+		if _, err := base64.StdEncoding.Decode(destination, content); err != nil {
+			return nil, err
+		}
+		return destination, nil
+	case FormatPlainText:
+		return content, nil
+	default:
+		return nil, fmt.Errorf("unsupported format: %s", format)
+	}
+}
+
+func ValidateFormat(format string) bool {
+	return format == FormatBase64 || format == FormatPlainText
 }
 
 var _ HandlerInterface = (*Handler)(nil)
