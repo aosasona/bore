4f4ca170704a5b7a28bd97cdf85174f0d52c2317
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-06 21:13:01 +0100

    Added macos clipboard implementation

--- pkg/system/clipboard_darwin.go
+++ pkg/system/clipboard_darwin.go
@@ -1,26 +1,62 @@
-//go:build darwin
-
 package system
 
-type nativeClipboard struct{}
+import (
+	"bytes"
+	"context"
+	"errors"
+	"os/exec"
+	"time"
+)
+
+type nativeClipboard struct {
+	// Path to `pbcopy`
+	copyBinPath string
+
+	// Path to `pbpaste`
+	pasteBinPath string
+}
 
 func NewNativeClipboard() (NativeClipboardInterface, error) {
-	return &nativeClipboard{}, nil
+	var err error
+
+	n := new(nativeClipboard)
+
+	n.copyBinPath, err = exec.LookPath("pbcopy")
+	if n.copyBinPath == "" || err != nil {
+		return n, errors.New("`pbcopy` not available")
+	}
+
+	n.pasteBinPath, err = exec.LookPath("pbpaste")
+	if n.pasteBinPath == "" || err != nil {
+		return n, errors.New("`pbpaste` not available")
+	}
+
+	return n, nil
 }
 
 // IsAvailable checks if a clipboard is available on the current system
 func (n *nativeClipboard) IsAvailable() bool {
-	panic("not implemented")
+	return (n.copyBinPath != "" && n.pasteBinPath != "")
 }
 
 // Copy copies the content to the system clipboard
 func (n *nativeClipboard) Copy(content []byte) error {
-	panic("not implemented")
+	ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, n.copyBinPath)
+	cmd.Stdin = bytes.NewReader(content)
+
+	return cmd.Run()
 }
 
 // Paste returns the last copied content from the system clipboard
 func (n *nativeClipboard) Paste() ([]byte, error) {
-	panic("not implemented")
+	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, n.pasteBinPath)
+	return cmd.Output()
 }
 
 var _ NativeClipboardInterface = (*nativeClipboard)(nil)
