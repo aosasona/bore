1f6f0565391161add37a35a9dd90e58970a85d63
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-09 22:41:50 +0100

    Init bore instance package

--- bore.go
+++ bore.go
@@ -1,1 +1,37 @@
 package bore
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/uptrace/bun"
+)
+
+type (
+	Bore struct {
+		// connection is the database connection used by this bore instance
+		db *bun.DB
+	}
+
+	Args struct {
+		// DataPath is the path to the storage directory.
+		DataPath string
+	}
+)
+
+var (
+	ErrInvalidArgs         = errors.New("invalid arguments provided to New function")
+	ErrStoragePathRequired = errors.New("storage path is required")
+)
+
+func New(args *Args) (*Bore, error) {
+	if args == nil {
+		return nil, ErrInvalidArgs
+	}
+
+	if strings.TrimSpace(args.DataPath) == "" {
+		return nil, ErrStoragePathRequired
+	}
+
+	panic("todo")
+}
