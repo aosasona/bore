2aba6597cc6d3aaca9977f2ae387b5a9f39ad578
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-13 00:41:55 +0100

    Add tests for system clipboard

---
+++ clipboard/clipboard_test.go
@@ -0,0 +1,42 @@
+package clipboard_test
+
+import (
+	"context"
+	"testing"
+
+	"go.trulyao.dev/bore/v2/clipboard"
+)
+
+func Test_IsAvailable(t *testing.T) {
+	c, err := clipboard.NewNativeClipboard()
+	if err != nil {
+		t.Fatalf("Failed to create clipboard: %v", err)
+	}
+
+	available := c.Available()
+	if !available {
+		t.Error("Expected clipboard to be available, but it is not")
+	}
+}
+
+func Test_SetAndGet(t *testing.T) {
+	c, err := clipboard.NewNativeClipboard()
+	if err != nil {
+		t.Fatalf("Failed to create clipboard: %v", err)
+	}
+
+	testText := "Hello, Clipboard!"
+	err = c.Write(context.TODO(), []byte(testText))
+	if err != nil {
+		t.Fatalf("Failed to set clipboard text: %v", err)
+	}
+
+	gotText, err := c.Read(context.TODO())
+	if err != nil {
+		t.Fatalf("Failed to get clipboard text: %v", err)
+	}
+
+	if string(gotText) != testText {
+		t.Errorf("Expected clipboard text '%s', got '%s'", testText, gotText)
+	}
+}
