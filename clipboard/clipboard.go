e2331a4216b8ad93d9b2397645f73a2092e881a3
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-12 21:41:53 +0100

    Take context in native clipboard methods

--- clipboard/clipboard.go
+++ clipboard/clipboard.go
@@ -1,12 +1,9 @@
 package system
 
-type Binaries struct {
-	// Path to the binary for copying to clipboard
-	Copy string
-
-	// Path to the binary for pasting from clipboard
-	Paste string
-}
+import (
+	"context"
+	"fmt"
+)
 
 // NativeClipboard is an interface that defines methods for common clipboard operations.
 type NativeClipboard interface {
@@ -14,15 +11,42 @@ type NativeClipboard interface {
 	Available() bool
 
 	// Write writes bytes to the clipboard.
-	Write(data []byte) error
+	Write(ctx context.Context, data []byte) error
 
 	// Read reads bytes from the clipboard.
-	Read() ([]byte, error)
+	Read(ctx context.Context) ([]byte, error)
 
 	// Clear clears the clipboard content.
-	Clear() error
+	Clear(ctx context.Context) error
 
 	// Binaries returns the paths to the (often) individual binaries used for clipboard operations
 	// For example; on MacOS, these might be `pbcopy` and `pbpaste`, while on Linux, they might be `xclip` or `xsel`.
 	Binaries() Binaries
+}
+
+type Binaries struct {
+	// Path to the binary for copying to clipboard
+	copy string
+
+	// Path to the binary for pasting from clipboard
+	paste string
+}
+
+func (b Binaries) Copy() string {
+	return b.copy
+}
+
+func (b Binaries) Paste() string {
+	return b.paste
+}
+
+func (b Binaries) Empty() bool {
+	return b.copy == "" && b.paste == ""
+}
+
+func (b Binaries) String() string {
+	if b.Empty() {
+		return "no binaries available"
+	}
+	return fmt.Sprintf("copy: %s, paste: %s", b.copy, b.paste)
 }
