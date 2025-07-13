e3808a8d82770e601f0552f4054191ed8ae82e31
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-13 01:09:08 +0100

    Remove older migrations

--- migrations/0001_create_collections_table.up.sql
+++ migrations/0001_create_collections_table.up.sql
@@ -1,11 +1,10 @@
 create table collections (
   id text primary key default (uuid()), -- uuid
-  name text not null,
-  is_folder_scoped integer not null default 0,
+  name text not null, -- the name of the collection
   folder_hash text, -- the folder names are not stored but the path hash is stored and used for lookup with "." as separator
-  last_modified integer not null default (unixepoch()),
+  created_at integer not null default (unixepoch()), -- timestamp in seconds
+  updated_at integer not null default (unixepoch()), -- timestamp in seconds
 
-  unique (folder_hash),
-  unique (name),
-  check (length(name) > 2)
+  unique (name, folder_hash)
 ) strict;
+
