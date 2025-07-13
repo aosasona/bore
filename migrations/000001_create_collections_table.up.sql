f6da866fbd15690717981a86056948c6b5dc9144
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-13 14:12:52 +0100

    Rename collections migration to match padding

--- migrations/000001_create_collections_table.up.sql
+++ migrations/000001_create_collections_table.up.sql
@@ -1,7 +1,8 @@
 create table collections (
-  id text primary key default (uuid()), -- uuid
+  id text primary key default (ulid()), -- uuid
   name text not null, -- the name of the collection
   folder_hash text, -- the folder names are not stored but the path hash is stored and used for lookup with "." as separator
+  pinned_at integer, -- timestamp in seconds when the collection was pinned
   created_at integer not null default (unixepoch()), -- timestamp in seconds
   updated_at integer not null default (unixepoch()), -- timestamp in seconds
 
