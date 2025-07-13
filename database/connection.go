96507e23562e6b26af5822f9502467064fd13dba
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-13 16:50:44 +0100

    Setup database connection

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,154 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/sqlitedialect"
+	"github.com/uptrace/bun/driver/sqliteshim"
+	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/uptrace/bun/migrate"
+	"go.trulyao.dev/bore/v2/migrations"
+)
+
+const DriverCustom = "sqlite3_custom"
+
+func isDev() bool {
+	return os.Getenv("APP_ENV") == "development" || strings.HasPrefix(os.Args[0], "go")
+}
+
+func Connect(dataDir string) (*bun.DB, error) {
+	connection, err := createDbConnection(dataDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create database connection: %w", err)
+	}
+
+	db := bun.NewDB(connection, sqlitedialect.New())
+	db.AddQueryHook(
+		bundebug.NewQueryHook(
+			bundebug.WithVerbose(true),
+			bundebug.WithEnabled(isDev()),
+		),
+	)
+
+	ctx := context.Background()
+	if err = runPragmas(ctx, db); err != nil {
+		return nil, fmt.Errorf("failed to run PRAGMAs: %w", err)
+	}
+
+	if err = runMigration(db); err != nil {
+		return nil, fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	return db, nil
+}
+
+func dsn(dataDir string) string {
+	path := filepath.Join(dataDir, "data.db")
+	return "file:" + path + "?_foreign_keys=on&mode=rwc&_journal_mode=WAL&cache=shared"
+}
+
+func createDbConnection(dataDir string) (*sql.DB, error) {
+	/*
+		if err := setupCustomFunctions(); err != nil {
+			return nil, err
+		}
+	*/
+
+	connection, err := sql.Open(sqliteshim.ShimName, dsn(dataDir))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	if err := connection.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return connection, nil
+}
+
+func runMigration(db *bun.DB) error {
+	ctx := context.Background()
+	migrator := migrate.NewMigrator(db, migrations.Migrations)
+
+	if err := migrator.Init(ctx); err != nil {
+		return err
+	}
+
+	if err := migrator.Lock(ctx); err != nil {
+		return err
+	}
+	defer migrator.Unlock(ctx)
+
+	groups, err := migrator.Migrate(ctx)
+	if err != nil {
+		return err
+	}
+
+	if groups.IsZero() {
+		fmt.Println("no new migrations to run")
+		return nil
+	}
+
+	fmt.Printf("migrated %d groups\n", len(groups.Migrations.Applied()))
+	return nil
+}
+
+func runPragmas(ctx context.Context, tx bun.IDB) error {
+	if _, err := tx.NewRaw("PRAGMA busy_timeout = 10000;").Exec(ctx); err != nil {
+		return err
+	}
+
+	if _, err := tx.NewRaw("PRAGMA foreign_keys = ON;").Exec(ctx); err != nil {
+		return err
+	}
+
+	if _, err := tx.NewRaw("PRAGMA journal_mode = WAL;").Exec(ctx); err != nil {
+		return err
+	}
+
+	if _, err := tx.NewRaw("PRAGMA synchronous = NORMAL;").Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// func setupCustomFunctions() error {
+// 	if slices.Contains(sql.Drivers(), DriverCustom) {
+// 		return errors.New("custom SQLite driver is already registered")
+// 	}
+//
+// 	sql.Register(DriverCustom, &sqlite3.SQLiteDriver{
+// 		ConnectHook: func(sc *sqlite3.SQLiteConn) error {
+// 			// ULID function
+// 			if err := sc.RegisterFunc("ulid", ulid, true); err != nil {
+// 				return fmt.Errorf("failed to register ulid function: %w", err)
+// 			}
+//
+// 			// SHA256 function
+// 			if err := sc.RegisterFunc("sha256", sha256, true); err != nil {
+// 				return fmt.Errorf("failed to register sha256 function: %w", err)
+// 			}
+//
+// 			return nil
+// 		},
+// 	})
+//
+// 	return nil
+// }
+//
+// func ulid() string {
+// 	return ulidv2.Make().String()
+// }
+//
+// func sha256(input string) string {
+// 	c := sha256mod.New()
+// 	c.Write([]byte(input))
+// 	return fmt.Sprintf("%x", c.Sum(nil))
+// }
--- go.mod
+++ go.mod
@@ -2,13 +2,31 @@ module go.trulyao.dev/bore/v2
 
 go 1.24.4
 
-require github.com/uptrace/bun v1.2.14
+require (
+	github.com/uptrace/bun v1.2.14
+	github.com/uptrace/bun/dialect/sqlitedialect v1.2.14
+	github.com/uptrace/bun/driver/sqliteshim v1.2.14
+	github.com/uptrace/bun/extra/bundebug v1.2.14
+)
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	modernc.org/libc v1.65.10 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
+	modernc.org/sqlite v1.38.0 // indirect
 )
