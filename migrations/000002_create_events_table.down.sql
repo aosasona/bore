e19d9e5f751fa3a4968ddc9d2814370764d1a342
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-13 15:12:32 +0100

    Add events table migration

---
+++ migrations/000002_create_events_table.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF EXISTS `events_ts_idx`;
+
+-- bun:split
+DROP INDEX IF EXISTS `events_agg_idx`;
+
+-- bun:split
+DROP TABLE IF EXISTS `events`;
+
---
+++ migrations/000002_create_events_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS events (
+	id         TEXT PRIMARY KEY DEFAULT (ulid()),
+	aggregate_id TEXT NOT NULL, -- the id of the aggregate this event belongs to
+	action     TEXT NOT NULL,
+	version    INTEGER NOT NULL DEFAULT 1,
+	payload    BLOB NOT NULL,
+	device_id  TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+	UNIQUE (aggregate_id, version) -- ensure that each event for an aggregate has a unique version
+) STRICT;
+
+-- bun:split
+CREATE INDEX IF NOT EXISTS events_agg_idx ON events (action, created_at);
+
+-- bun:split
+CREATE INDEX IF NOT EXISTS events_ts_idx ON events (created_at);
+
