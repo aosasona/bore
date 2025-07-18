03f9250f86175fd95823aa37365a3b93d9013dea
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-07-18 23:56:59 +0100

    Remove DataDir and ConfigPath in config struct

--- cmd/bore/root.go
+++ cmd/bore/root.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/urfave/cli/v2"
+)
+
+// TODO: move path to the CLI itself (can be overriden by using the -c flag)
+// TODO: automatically detect the data directory for each platform and remove this config option
 func execute() error {
 	panic("not implemented")
 }
