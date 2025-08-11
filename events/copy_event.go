c4586d73ab7cd48e4e4314f878343a2cc73e5da0
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-11 19:46:19 +0100

    Implement Marshal and Unmarshal for copy (V1) event

--- events/copy_event.go
+++ events/copy_event.go
@@ -1,6 +1,11 @@
 package events
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/uptrace/bun"
 	"go.trulyao.dev/bore/v2/models"
 )
@@ -8,16 +13,16 @@ import (
 // copyEvent is emitted when a copy operation is performed.
 type copyEvent struct {
 	// The content that was copied to the clipboard
-	Content []byte `json:"content"`
+	Content []byte `json:"content" mapstructure:"content"`
 
 	// Hash is the SHA256 hash of the content that was copied.
-	Hash string `json:"hash"`
+	Hash string `json:"hash" mapstructure:"hash"`
 
 	// MimeType is the MIME type of the content that was copied.
-	MimeType MimeType `json:"mime_type"`
+	MimeType MimeType `json:"mime_type" mapstructure:"mime_type"`
 
 	// CollectionID is the identifier of the collection to which this event belongs.
-	CollectionID string `json:"collection_id"`
+	CollectionID string `json:"collection_id" mapstructure:"collection_id"`
 }
 
 // Action implements Event.
@@ -37,12 +42,42 @@ func (c *copyEvent) Apply(db *bun.DB) (Log, error) {
 
 // MarshalJSON implements Event.
 func (c *copyEvent) MarshalJSON() ([]byte, error) {
-	panic("unimplemented")
+	return json.Marshal(map[string]any{
+		"content":       c.Content,
+		"hash":          c.Hash,
+		"mime_type":     c.MimeType.String(),
+		"collection_id": c.CollectionID,
+	})
 }
 
 // UnmarshalJSON implements Event.
 func (c *copyEvent) UnmarshalJSON([]byte) error {
-	panic("unimplemented")
+	data := map[string]any{}
+	if err := json.Unmarshal([]byte{}, &data); err != nil {
+		return err
+	}
+
+	if err := mapstructure.Decode(data, c); err != nil {
+		return fmt.Errorf("failed to decode copy event: %w", err)
+	}
+
+	if c.Content == nil || len(c.Content) == 0 {
+		return errors.New("content cannot be empty")
+	}
+
+	if c.Hash == "" {
+		return errors.New("hash cannot be empty")
+	}
+
+	if c.MimeType == nil {
+		return errors.New("mime type cannot be nil")
+	}
+
+	if c.CollectionID == "" {
+		return errors.New("collection ID cannot be empty")
+	}
+
+	return nil
 }
 
 var _ Event = (*copyEvent)(nil)
