7adeb4f98d411f9c2fe019c3c3d76a6365f32b54
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-09-18 22:41:57 +0100

    Split aggregate in events table

--- migrations/000002_create_events_table.down.sql
+++ migrations/000002_create_events_table.down.sql
@@ -1,13 +1,16 @@
+DROP INDEX IF EXISTS idx_events_stream_seq;
+
+-- bun:split
 DROP INDEX IF EXISTS idx_events_sequence_id;
 
 -- bun:split
 DROP INDEX IF EXISTS idx_events_type;
 
 -- bun:split
-DROP INDEX IF EXISTS idx_events_aggregate_version;
+DROP INDEX IF EXISTS idx_events_stream;
 
 -- bun:split
-DROP INDEX IF EXISTS idx_events_aggregate;
+DROP INDEX IF EXISTS ux_events_stream_version;
 
 -- bun:split
 DROP TABLE IF EXISTS events;
--- migrations/000002_create_events_table.up.sql
+++ migrations/000002_create_events_table.up.sql
@@ -1,23 +1,31 @@
 CREATE TABLE IF NOT EXISTS events (
 	sequence_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	event_id TEXT NOT NULL UNIQUE,
-	aggregate TEXT, -- e.g. 'item:<item_id>'
-	aggregate_version INTEGER,
+
+	aggregate_type TEXT NOT NULL, -- e.g. 'item'
+	aggregate_id TEXT NOT NULL,   -- e.g. valid ULID
+	aggregate_version INTEGER NOT NULL, -- version of the aggregate after this event (1 to N)
+
 	type TEXT NOT NULL,
-	payload JSON NOT NULL,
+	payload BLOB NOT NULL CHECK (json_valid(payload)),
 -- use ISO8601 format for timestamps
-	occurred_at TEXT NOT NULL DEFAULT (datetime('now'))
+	occurred_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
+
+	stream_id TEXT GENERATED ALWAYS AS (aggregate_type || ':' || aggregate_id) STORED
 );
 
 -- bun:split
-CREATE INDEX IF NOT EXISTS idx_events_aggregate ON events(aggregate);
+CREATE UNIQUE INDEX IF NOT EXISTS ux_events_stream_version ON events(aggregate_type, aggregate_id, aggregate_version);
 
 -- bun:split
-CREATE INDEX IF NOT EXISTS idx_events_aggregate_version ON events(aggregate, aggregate_version);
+CREATE INDEX IF NOT EXISTS idx_events_stream ON events(stream_id);
 
 -- bun:split
 CREATE INDEX IF NOT EXISTS idx_events_type ON events(type);
 
 -- bun:split
 CREATE INDEX IF NOT EXISTS idx_events_sequence_id ON events(sequence_id);
+
+-- bun:split
+CREATE INDEX IF NOT EXISTS idx_events_stream_seq ON events(aggregate_type, aggregate_id, sequence_id);
 
