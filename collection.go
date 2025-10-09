a033b44aa90dd40b92d2eaee03bb0c7cb53311fd
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-10-09 23:31:36 +0100

    Implement Rename in Collections namespace

--- collection.go
+++ collection.go
@@ -70,7 +70,34 @@ func (c *collectionNamespace) Delete(ctx context.Context, identifier string) err
 // Rename renames a collection. This does not change the collection ID.
 // It will return an error if the new name is already taken by another collection.
 func (c *collectionNamespace) Rename(ctx context.Context, identifier, newName string) error {
-	panic("not implemented")
+	if !validation.IsValidCollectionName(newName) {
+		return validation.ErrInvalidCollectionName
+	}
+
+	existingCollection, err := c.repository.Collections().FindByName(ctx, newName)
+	if err != nil {
+		return errs.New("failed to check existing collection name").WithError(err)
+	}
+
+	if existingCollection != nil {
+		return errs.New("collection with the same name already exists")
+	}
+
+	agg, err := aggregate.WithID(aggregate.AggregateTypeCollection, identifier)
+	if err != nil {
+		return errs.New("failed to create aggregate for rename event").WithError(err)
+	}
+
+	event, err := events.New(agg, &payload.RenameCollection{NewName: newName})
+	if err != nil {
+		return errs.New("failed to create collection rename event").WithError(err)
+	}
+
+	if _, _, err = c.manager.Apply(ctx, event); err != nil {
+		return errs.New("failed to apply collection rename event").WithError(err)
+	}
+
+	return nil
 }
 
 // List returns all collections with their associated items count.
