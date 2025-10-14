3e9b5a223cca676aa3c2ccd5db31aeb543113fe5
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-10-14 21:43:21 +0100

    Implement `CreateCollection` handler for CLI

--- cmd/bore-cli/app/handler/collections.go
+++ cmd/bore-cli/app/handler/collections.go
@@ -1,13 +1,34 @@
 package handler
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+	"go.trulyao.dev/bore/v2"
+)
 
 func (h *Handler) ListCollections(c *cli.Context) error {
+	format := c.String(FlagFormat)
+	if format == "" {
+		format = string(PasteFormatText)
+	}
+
+	_ = format
 	panic("not implemented")
 }
 
 func (h *Handler) CreateCollection(c *cli.Context) error {
-	panic("not implemented")
+	name := c.Args().First()
+	force := c.Bool(FlagForce)
+
+	result, err := h.bore.Collections().Create(
+		c.Context,
+		bore.CreateCollectionOptions{Name: name, AppendSuffixIfExists: force},
+	)
+	if err != nil {
+		return cli.Exit("failed to create collection: "+err.Error(), 1)
+	}
+
+	_, _ = c.App.Writer.Write([]byte(result.Name + " (" + result.ID + ")\n"))
+	return nil
 }
 
 func (h *Handler) DeleteCollection(c *cli.Context) error {
