9023bbd33dfc73a8121ced7f8f3bf9c95a93b7e5
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2026-06-21 06:12:59 +0100

    Ignore clipboard requirements on WSL in tests
    On WSL, we don't have any of the Linux clipboards available and it will
    always cause the Test_SetAndGet test to fail.

--- pkg/clipboard/clipboard_test.go
+++ pkg/clipboard/clipboard_test.go
@@ -2,12 +2,38 @@ package clipboard_test
 
 import (
 	"context"
+	"os"
+	"runtime"
+	"strings"
 	"testing"
 
 	"go.trulyao.dev/bore/v2/pkg/clipboard"
 )
 
+func skipOnWSL(t *testing.T) {
+	t.Helper()
+
+	if runtime.GOOS != "linux" {
+		return
+	}
+
+	if os.Getenv("WSL_INTEROP") != "" || os.Getenv("WSL_DISTRO_NAME") != "" {
+		t.Skip("skipping native clipboard test on WSL")
+	}
+
+	version, err := os.ReadFile("/proc/version")
+	if err != nil {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(string(version)), "microsoft") {
+		t.Skip("skipping native clipboard test on WSL")
+	}
+}
+
 func Test_IsAvailable(t *testing.T) {
+	skipOnWSL(t)
+
 	c, err := clipboard.NewNativeClipboard()
 	if err != nil {
 		t.Fatalf("Failed to create clipboard: %v", err)
@@ -20,6 +46,8 @@ func Test_IsAvailable(t *testing.T) {
 }
 
 func Test_SetAndGet(t *testing.T) {
+	skipOnWSL(t)
+
 	c, err := clipboard.NewNativeClipboard()
 	if err != nil {
 		t.Fatalf("Failed to create clipboard: %v", err)
